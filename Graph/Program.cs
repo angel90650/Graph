428800695fe7953934f475dafd67462955d051c8
Angel Castro <angelcastro@umail.ucsb.edu>
2018-08-02 23:01:11 -0700

    added AdjacencyList ,add and print

--- Graph/Program.cs
+++ Graph/Program.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Graph
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int size = 20;
+            AdjacencyList graph = new AdjacencyList(size);
+            graph.AddEdge(15, 4);
+            graph.AddEdge(0, 2);
+            graph.AddEdge(1, 4);
+            graph.AddEdge(3, 4);
+            graph.AddEdge(4, 5);
+            graph.AddEdge(8, 4);
+            graph.AddEdge(4, 2);
+            graph.print();
+
+            Console.ReadLine();
+                
         }
     }
 }
---
+++ Graph/adjacencyList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    class AdjacencyList
+    {
+        private List<int> [] list;
+        private int numEdges;
+        private int numVertices;
+        private int size;
+
+        public AdjacencyList(int size)
+        {
+            this.size = size;
+            list = new List<int>[size];
+            for(int i = 0; i < size; i++)
+            {
+                list[i] = new List<int>();
+            }
+            numEdges = 0;
+            numVertices = 0;
+        }
+
+        public void AddEdge(int src, int dest)
+        {
+            if (src < size && dest < size)
+            {
+                if (list[src].Count == 0) numVertices++;
+                if (list[dest].Count == 0) numVertices++;
+                list[src].Add(dest);
+                list[dest].Add(src);
+                numEdges++;
+            }
+            else
+            {
+                throw new Exception("vectors are not within size of adjacency list");
+            }
+        }
+
+        public void print()
+        {
+            Console.WriteLine("Printing Graph with " + numEdges + " edges and " + numVertices + " vertices");
+            int src = 0;
+            foreach(List<int> x in list)
+            {
+                if(x.Count != 0)
+                {
+                    foreach(int dest in x)
+                    {
+                        Console.WriteLine("Edge (" + src + " - " + dest + ")");
+                    }
+                }
+                src++;
+            }
+        }
+    }
+}
